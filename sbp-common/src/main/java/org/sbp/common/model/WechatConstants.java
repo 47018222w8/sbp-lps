d5ff0c7c3845ce9cd17378e4157dbed6ee3300a8
47018222w8 <1623754384@qq.com>
2017-10-20 10:20:59 +0800

    Update WechatConstants.java

--- sbp-common/src/main/java/org/sbp/common/model/WechatConstants.java
+++ sbp-common/src/main/java/org/sbp/common/model/WechatConstants.java
@@ -3,7 +3,7 @@ package org.sbp.common.model;
 
 public class WechatConstants {
  // 微信推送模板secret
-    public static final String TEMPLATE_MESSAGE_SECRET = "G0GSWV5GCYUP7FC7KD";
+    public static final String TEMPLATE_MESSAGE_SECRET = "";
 
     // 微信推送url
     public static final String WECHAT_TEMPLATE_MESSAGE_URL = "http://www.eauto100.com/wxservice/api/v1/messages";
